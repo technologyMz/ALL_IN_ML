e1e43155b3bbdce7932cace2fdbdc8c12ce40ed4
yayaa <bityanghui@qq.com>
2021-08-12 21:14:49 +0800

    doing

--- yanghui/733floodFill.py
+++ yanghui/733floodFill.py
@@ -1,9 +1,37 @@
 class Solution:
     def floodFill(self, image, sr: int, sc: int, newColor: int):
+        def check_point(x,y, image):
+            if 0 <=x < len(image) and 0 <= y < len(image[0]) and x :
+                return True
+            return False
+
         the_Color = image[sr][sc]
         pixel_list = []
-        open_list = [the_Color]
-        while len(open_list) == 0:
+        open_list = [[sr,sc]]
+        while len(open_list) != 0:
             for one_pixel in open_list:
+                print(open_list)
                 if image[one_pixel[0]][one_pixel[1]] == the_Color:
                     pixel_list.append(one_pixel)
+                    image[one_pixel[0]][one_pixel[1]] = newColor
+                    open_list.remove(one_pixel)
+                    add_list = [[one_pixel[0] - 1,  one_pixel[1]],
+                                [one_pixel[0] + 1, one_pixel[1]],
+                                [one_pixel[0], one_pixel[1] - 1],
+                                [one_pixel[0], one_pixel[1] + 1]]
+                    for point in add_list:
+                        if check_point(point[0], point[1], image):
+                            # print(point)
+                            if image[point[0]][point[1]] == the_Color and point not in pixel_list and point not in open_list:
+                                open_list.append(point)
+                                # print(point)
+        return image
+
+
+
+image = [[1,1,1],[1,1,0],[1,0,1]]
+sr = 1
+sc = 1
+newColor = 2
+solution = Solution()
+print(solution.floodFill(image,sr,sc,newColor))
