2642a0afc2f2614d2abbb8ad9b27dba5c3b22762
yayaa <bityanghui@qq.com>
2021-08-13 13:02:53 +0800

    没用队列世界太长

--- yanghui/695maxAreaOfIsland.py
+++ yanghui/695maxAreaOfIsland.py
@@ -9,9 +9,6 @@ class Solution:
         for seed in map_list:
             if seed in used_point:
                 continue
-            # print('len(map_list)', len(map_list))
-            if seed == [3,8]:
-                print('the one')
             earth_points = []
             open_list = []
             if grid[seed[0]][seed[1]] == 1:
@@ -20,19 +17,19 @@ class Solution:
                 while open_list:
                     for one_point in open_list:
                         # print(one_point)
-                        if grid[one_point[0]][one_point[1]] == 1:
-                            earth_points.append(one_point)
-                            open_list.remove(one_point)
-                            max_area = len(earth_points) if len(earth_points) > max_area else max_area
-                            nearby_points = [[-1, 0], [+1, 0], [0, -1], [0, +1]]
-                            for one_nearby in nearby_points:
-                                one_nearby = [one_point[0] + one_nearby[0], one_point[1] + one_nearby[1]]
-                                # print(one_nearby)
-                                if 0 <= one_nearby[0]  < len(seed) and 0 <= one_nearby[1] < len(grid[0]) and one_nearby in map_list:
-                                    if grid[one_nearby[0]][one_nearby[1]] == 1:
-                                        if one_nearby not in open_list and one_nearby not in earth_points:
-                                            open_list.append(one_nearby)
-                                    used_point.append(one_nearby)
+                        earth_points.append(one_point)
+                        open_list.remove(one_point)
+                        max_area = len(earth_points) if len(earth_points) > max_area else max_area
+                        nearby_points = [[-1, 0], [+1, 0], [0, -1], [0, +1]]
+                        for one_nearby in nearby_points:
+                            one_nearby = [one_point[0] + one_nearby[0], one_point[1] + one_nearby[1]]
+                            # print(one_nearby, grid[one_nearby[0]][one_nearby[1]])
+                            if 0 <= one_nearby[0] < len(grid) and 0 <= one_nearby[1] < len(grid[0]) and one_nearby in map_list:
+                                if grid[one_nearby[0]][one_nearby[1]] == 1:
+                                    if one_nearby not in open_list and one_nearby not in earth_points:
+                                        open_list.append(one_nearby)
+                                used_point.append(one_nearby)
+                        used_point.append(one_point)
             used_point.append(seed)
         return max_area
 
