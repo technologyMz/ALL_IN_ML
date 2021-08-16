c0c9a9f7b41848d70c765ae5fb5a81934525a934
yayaa <bityanghui@qq.com>
2021-08-16 21:17:22 +0800

    增加队列方法，debuging

--- yanghui/733floodFill.py
+++ yanghui/733floodFill.py
@@ -5,13 +5,13 @@ class Solution:
                 return True
             return False
 
-        the_Color = image[sr][sc]
+        oldColor = image[sr][sc]
         pixel_list = []
         open_list = [[sr,sc]]
         while len(open_list) != 0:
             for one_pixel in open_list:
                 print(open_list)
-                if image[one_pixel[0]][one_pixel[1]] == the_Color:
+                if image[one_pixel[0]][one_pixel[1]] == oldColor:
                     pixel_list.append(one_pixel)
                     image[one_pixel[0]][one_pixel[1]] = newColor
                     open_list.remove(one_pixel)
@@ -22,16 +22,31 @@ class Solution:
                     for point in add_list:
                         if check_point(point[0], point[1], image):
                             # print(point)
-                            if image[point[0]][point[1]] == the_Color and point not in pixel_list and point not in open_list:
+                            if image[point[0]][point[1]] == oldColor and point not in pixel_list and point not in open_list:
                                 open_list.append(point)
                                 # print(point)
         return image
 
+    def floodFill_2(self, image, sr: int, sc: int, newColor: int): # 用队列
+        import collections
+        oldColor = image[sr][sc]
+        due = collections.deque([[sr, sc]])
+        while due:
+            x, y = due.popleft()
+            image[x][y] = newColor
+            for mx, my in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+                if 0 <= mx < len(image) and 0 <= my < len(image[0]) and image[mx][my] == oldColor:
+                    image[mx][my] = newColor
+                    due.append([mx,my])
+        return image
 
 
-image = [[1,1,1],[1,1,0],[1,0,1]]
+
+
+image = [[0,0,0],[0,1,1]]
 sr = 1
 sc = 1
-newColor = 2
+newColor = 1
 solution = Solution()
-print(solution.floodFill(image,sr,sc,newColor))
+# print(solution.floodFill(image,sr,sc,newColor))
+print(solution.floodFill_2(image,sr,sc,newColor))
